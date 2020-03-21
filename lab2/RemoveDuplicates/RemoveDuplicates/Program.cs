1efa632321ae7c73b5fdea0c6f8f96eaa05d97ab
Carapacik <carapaciK@gmail.com>
2020-03-21 16:19:29 +0300

    commit with .gitignore

---
+++ lab2/RemoveDuplicates/RemoveDuplicates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage RemoveDuplicates.exe <input string>");
+            }
+            else
+            {
+                HashSet<char> setOfChar = new HashSet<char>();
+                string inputStr = args[0];
+                char tempCh;
+                for (int i = 0; i < inputStr.Length; i++)
+                {
+                    tempCh = inputStr[i];
+                    if (setOfChar.Contains(tempCh)){
+                        inputStr = inputStr.Remove(i, 1);
+                        i--;
+                    }
+                    else
+                    {
+                        setOfChar.Add(tempCh);
+                    }
+                }
+
+                Console.WriteLine(inputStr);
+            }
+        }
+    } 
+}
