c1f9dd52e80ceca7b8b787930b8e2dea64480033
Carapacik <carapaciK@gmail.com>
2020-03-28 15:19:30 +0300

    create task 3.1

---
+++ lab3/CheckIdentifier/CheckIdentifier/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CheckIdentifier
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect arguments count");
+
+                return 1;
+            }
+            else
+            {
+                string inputStr = args[0];
+                if (inputStr.Length > 0)
+                {
+                    if (!IsNumber(inputStr[0]))
+                    {
+                        for (int i = 0; i < inputStr.Length; i++)
+                        {
+                            if (!IsNumber(inputStr[i]) && !IsLetter(inputStr[i]))
+                            {
+                                Console.WriteLine("No");
+                                Console.WriteLine("The identifier must contain only numbers or letters.");
+
+                                return 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                        Console.WriteLine("Identifier can't start with a digit.");
+
+                        return 1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                    Console.WriteLine("An empty string was passed.");
+
+                    return 1;
+                }
+
+            }
+            Console.WriteLine("yes");
+
+            return 0;
+        }
+
+        public static bool IsNumber(char symbol)
+        {
+            return symbol >= '0' && symbol <= '9';
+        }
+
+        public static bool IsLetter(char symbol)
+        {
+            return (symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z');
+        }
+    }
+}
---
+++ lab3/CheckIdentifier/CheckidentifierTests/CheckIdentifierTests.cs
@@ -0,0 +1,138 @@
+﻿using CheckIdentifier;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckidentifierTests
+{
+    [TestClass]
+    public class CheckIdentifierTests
+    {
+        [TestMethod]
+        public void CheckIdentifierTests_IncorrectSymbol()
+        {
+            // Act
+            string[] wrongStr = new string[1]{ "Silens!" };
+
+            int endValue = Program.Main(wrongStr);
+
+            // Assert
+            Assert.AreEqual(1, endValue);
+
+        }
+
+        [TestMethod]
+        public void CheckIdentifierTests_IncorrectArgumentsCount()
+        {
+            // Act
+            string[] wrongStr = new string[2] { "Privet", "Alo" };
+
+            int endValue = Program.Main(wrongStr);
+
+            // Assert
+            Assert.AreEqual(1, endValue);
+        }
+
+        [TestMethod]
+        public void CheckIdentifierTests_StartWithDigit()
+        {
+            // Act
+            string[] wrongStr = new string[1] { "1Begin" };
+
+            int endValue = Program.Main(wrongStr);
+
+            // Assert
+            Assert.AreEqual(1, endValue);
+        }
+
+        [TestMethod]
+        public void CheckIdentifierTests_InputIsEmptyString()
+        {
+            // Act
+            string[] wrongStr = new string[1] {""};
+
+            int endValue = Program.Main(wrongStr);
+
+            // Assert
+            Assert.AreEqual(1, endValue);
+        }
+
+        [TestMethod]
+        public void CheckIdentifierTests_EmptyInput()
+        {
+            // Act
+            string[] wrongStr = new string[0] {};
+
+            int endValue = Program.Main(wrongStr);
+
+            // Assert
+            Assert.AreEqual(1, endValue);
+        }
+
+        [TestMethod]
+        public void CheckIdentifierTests_CorrectInput()
+        {
+            // Act
+            string[] correctStr = new string[1] { "Begin" };
+
+            int endValue = Program.Main(correctStr);
+
+            // Assert
+            Assert.AreEqual(0, endValue);
+        }
+
+        [TestMethod]
+        public void IsNumber_Number_ReturnTrue()
+        {
+            // Act
+            char number = '1';
+            bool resresult = CheckIdentifier.Program.IsNumber(number);
+
+            // Assert
+            Assert.AreEqual(true, resresult);
+        }
+
+        [TestMethod]
+        public void IsNumber_NotNumber_ReturnTrue()
+        {
+            // Act
+            char notnumber = '[';
+            bool resresult = CheckIdentifier.Program.IsNumber(notnumber);
+
+            // Assert
+            Assert.AreEqual(false, resresult);
+        }
+
+        [TestMethod]
+        public void IsLetter_WithLetterUpperCase_ReturnTrue()
+        {
+            // Act
+            char letter = 'B';
+            bool resresult = CheckIdentifier.Program.IsLetter(letter);
+
+            // Assert
+            Assert.AreEqual(true, resresult);
+        }
+
+
+        [TestMethod]
+        public void IsLetter_WithLetterLowerCase_ReturnTrue()
+        {
+            // Act
+            char letter = 'c';
+            bool resresult = CheckIdentifier.Program.IsLetter(letter);
+
+            // Assert
+            Assert.AreEqual(true, resresult);
+        }
+
+        [TestMethod]
+        public void IsLetter_WithNotLetter_ReturnFalse()
+        {
+            // Act
+            char notletter = '[';
+            bool resresult = CheckIdentifier.Program.IsLetter(notletter);
+
+            // Assert
+            Assert.AreEqual(false, resresult);
+        }
+    }
+}
